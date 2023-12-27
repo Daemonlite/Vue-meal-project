571f8e5d2eba989acea57161ba359e7e202bdf5e
Daemonlite <khemikal2016@gmail.com>
2023-12-27 16:59:03 +0000

    view

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,7 @@
 <script setup></script>
 
 <template>
-  <div class="bg-gray-100 h-full">
-    <header class="bg-white shadow h-16 flex justify-between items-stretch ">
-    <router-link :to="{ name: 'home' }" class="inline-flex items-center px-5 h-full ">
-     Home
-    </router-link>
-   
-    <div class="flex items-center">
-      <router-link :to="{ name: 'byName' }" class="inline-flex items-center px-2 h-full transition-colors hover:bg-purple-100">
-       Search Meals by name
-       </router-link>
-
-      <router-link :to="{ name: 'byLetter' }" class="inline-flex items-center px-2 h-full transition-colors hover:bg-purple-100">
-       Meals by Letter  
-      </router-link>
-
-      <router-link :to="{ name: 'byIngredient' }" class="inline-flex items-center px-2 h-full transition-colors hover:bg-purple-100">
-        Meals by Ingredients
-      </router-link>
-    </div>
-
-    </header>
-    <main>
-      <router-view />
-    </main>
-  </div>
+<router-view/>
 </template>
 
 <style scoped></style>
--- src/router/index.js
+++ src/router/index.js
@@ -3,31 +3,46 @@ import Home from '../views/Home.vue'
 import MealsByName from '../views/MealsByName.vue'
 import MealsByIngredients from '../views/MealsByIngredients.vue'
 import MealsByLetter from '../views/MealsByLetter.vue'
+import DefaultLayout from '../components/DefaultLayout.vue'
+import GuestLayout from '../components/GuestLayout.vue'
 
 const routes = [
     {
         path:'/',
-        name:'home',
-        component:Home
-    },
-    {
-        path:'/letter/:letter?',
-        name:'byLetter',
-        component:MealsByLetter
-    },
-    {
-        path:'/name/:name?',
-        name:'byName',
-        component:MealsByName
+        component:DefaultLayout,
+        children:[
+            {
+                path:'/',
+                name:'home',
+                component:Home
+            },
+            {
+                path:'/letter/:letter?',
+                name:'byLetter',
+                component:MealsByLetter
+            },
+            {
+                path:'/name/:name?',
+                name:'byName',
+                component:MealsByName
+        
+            },
+            {
+                path:'/ingredient/:ingredient?',
+                name:'byIngredient',
+                component:MealsByIngredients
+            },
+        
+        ]
 
     },
+   
+
     {
-        path:'/ingredient/:ingredient?',
-        name:'byIngredient',
-        component:MealsByIngredients
-    },
-
-
+        path:'/guest',
+        name:'guest',
+        component:GuestLayout
+    }
 ]
 
 const router = createRouter({
